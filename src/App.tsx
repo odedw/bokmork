5469dc9035e34cabc0daaa000b0220f13526341d
Oded Welgreen <oded.welgreen@gmail.com>
2019-09-18 09:10:03 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { withAuthenticator } from 'aws-amplify-react';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
