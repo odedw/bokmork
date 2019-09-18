da217165ea9c9737a870a1987c05803804ab6763
Oded Welgreen <oded.welgreen@gmail.com>
2019-09-18 08:50:44 -0500

    Updates

---
+++ precache-manifest.73bf9b980734994c347e7f2df47762e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25083a1dbea3de1a8d56ffb2bf4520d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab8b94864422bde9b792",
+    "url": "/static/css/2.eee0f4a3.chunk.css"
+  },
+  {
+    "revision": "d795ff8cb1bc5a71d1a1",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "ab8b94864422bde9b792",
+    "url": "/static/js/2.2aa72896.chunk.js"
+  },
+  {
+    "revision": "d795ff8cb1bc5a71d1a1",
+    "url": "/static/js/main.3c73bfaf.chunk.js"
+  },
+  {
+    "revision": "15b46b0ad93ee98b209a",
+    "url": "/static/js/runtime~main.0c53bf72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
